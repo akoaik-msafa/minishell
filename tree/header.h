2f09bb1811416f07710b30dc915975e51b4d5b5b
Ali Koaik <akoaik@c1r9s10.42beirut.com>
2025-09-05 22:40:38 +0300

    tree

--- tree/header.h
+++ tree/header.h
@@ -6,9 +6,13 @@
 /*   By: akoaik <akoaik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 03:26:26 by akoaik            #+#    #+#             */
-/*   Updated: 2025/09/05 03:29:15 by akoaik           ###   ########.fr       */
+/*   Updated: 2025/09/05 18:43:07 by akoaik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 struct tree {
 	int value ;
