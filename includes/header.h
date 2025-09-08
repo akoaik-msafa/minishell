9f7ebf5da88986309c536db06abdc7d33d27505a
Mohammad Safa <msafa@c1r3s7.42beirut.com>
2025-09-08 20:54:16 +0300

    echo

--- includes/header.h
+++ includes/header.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: msafa <msafa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/05 22:47:33 by msafa             #+#    #+#             */
-/*   Updated: 2025/09/05 22:47:34 by msafa            ###   ########.fr       */
+/*   Created: 2025/09/07 15:31:25 by msafa             #+#    #+#             */
+/*   Updated: 2025/09/08 20:24:38 by msafa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,14 @@
 #include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+
+typedef int (*built_in)(char **args);
+
+typedef struct s_built_in{
+	 char *cmd;
+	 built_in func;
+} t_builtin;
+
+int ft_echo(char **args);
+int ft_strcmp(const char *s1, const char *s2);
+int check_options(char *arg);
