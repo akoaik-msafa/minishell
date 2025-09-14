a02f689c06387502c57a3a419c17f482b930b76a
Mohammad Safa <msafa@c1r1s3.42beirut.com>
2025-09-14 16:01:14 +0300

    SAFA

--- includes/header.h
+++ includes/header.h
@@ -18,24 +18,33 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-typedef int	(*t_built_in)(char *arg);
+typedef int	(*t_simple_func)(char *arg);
+typedef int	(*t_env_func)(char *arg, char **env);
 
-typedef struct s_built_in
+typedef struct s_simple_builtin
 {
 	char		*cmd;
-	t_built_in	func;
-}	t_builtin;
+	t_simple_func	func;
+}	t_simple_builtin;
+
+typedef struct s_env_builtin
+{
+	char		*cmd;
+	t_env_func	func;
+}	t_env_builtin;
 
 /* Builtin functions */
 int		ft_echo(char *arg);
 int		ft_cd(char *arg);
 int		ft_pwd(char *arg);
-int		ft_export(char *arg);
-int		ft_unset(char *arg);
-int		ft_env(char *arg);
-int		ft_exit(char *arg);
+int		ft_export(char *arg, char **env);
+int		ft_unset(char *arg, char **env);
+int		ft_env(char *arg, char **env);
+int		ft_exit(char *arg, char **env);
 
 /* Helper functions */
+char	*ft_strncpy(char *dest, const char *src, int n);
+char	*ft_strdup(const char *s);
 int		ft_strcmp(const char *s1, const char *s2);
 int		count_words(char *str);
 void	skip_spaces(char *str, int *i);
@@ -62,4 +71,11 @@ char	*get_cd_path(char *arg, char ***args_ptr);
 void	print_cwd(const char *label);
 
 /* Main functions */
-void	init_builtins(t_builtin *builtins);
+void	init_simple_builtins(t_simple_builtin *simple_builtins);
+void	init_env_builtins(t_env_builtin *env_builtins);
+int		search_simple_builtins(char *cmd, char *args_str, t_simple_builtin *simple_builtins);
+int		search_env_builtins(char *cmd, char *args_str, 
+			t_env_builtin *env_builtins, char **env);
+int		execute_builtin(char *input, char **env);
+char	**duplicate_env(char **env);
+void	free_env(char **env);
