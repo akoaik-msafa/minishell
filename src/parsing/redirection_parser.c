e00e2cce2194ec91e93e838b7bfce699b04b7b0d
Ali Koaik <akoaik@c1r5s7.42beirut.com>
2025-10-25 20:37:15 +0300

    25OCT

--- src/parsing/redirection_parser.c
+++ src/parsing/redirection_parser.c
@@ -6,7 +6,7 @@
 /*   By: akoaik <akoaik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 23:14:12 by msafa             #+#    #+#             */
-/*   Updated: 2025/10/22 18:43:12 by akoaik           ###   ########.fr       */
+/*   Updated: 2025/10/25 20:35:38 by akoaik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,53 +86,3 @@ tree_node	*handle_redirection_parsing(token_t *tokens, token_t *redir_pos,
 	else
 		return (handle_default_case(left_node, right_node, redir_pos, data));
 }
-
-/* SIMPLIFIED VERSION - COMPARE WITH ABOVE */
-
-tree_node	*handle_redirection_parsing_v2(token_t *tokens, token_t *redir_pos,
-		token_t *end, t_data *data)
-{
-	tree_node	*left_node;
-	tree_node	*right_node;
-	int			left_count;
-	int			right_count;
-
-	// Validate: redirection must have a filename after it (e.g., "< file")
-	if (redir_pos + 1 >= end || (redir_pos + 1)->type != t_word)
-		return (NULL);
-
-	// Parse LEFT side: tokens BEFORE the redirection (e.g., "echo hello > file")
-	// left_count = number of tokens before redirection
-	left_count = redir_pos - tokens;
-	if (left_count > 0)
-		left_node = parse_tokens(tokens, left_count, data->n_head, data);
-	else
-		left_node = NULL;
-
-	// Parse RIGHT side: tokens AFTER the redirection filename (e.g., "< file cat")
-	// Skip 2 positions: redirection operator + filename
-	right_count = end - (redir_pos + 2);
-	if (right_count > 0)
-		right_node = parse_tokens(redir_pos + 2, right_count, data->n_head, data);
-	else
-		right_node = NULL;
-
-	// CASE 1: Right side is another redirection (e.g., "< file1 > file2")
-	// Chain redirections together with left_node at the deepest level
-	if (right_node && right_node->type == redir_node)
-		return (handle_redir_node_case(left_node, right_node, redir_pos, data));
-
-	// CASE 2: Both sides are commands (e.g., "echo hello > file cat")
-	// Merge the command arguments from both sides
-	if (right_node && right_node->type == cmd_node && left_node
-		&& left_node->type == cmd_node)
-		return (handle_cmd_node_case(left_node, right_node, redir_pos, data));
-
-	// CASE 3: Default case - simple redirection
-	// Prefer right_node if exists, otherwise use left_node
-	// Examples: "> file", "echo > file", "< file cat"
-	if (right_node)
-		return (create_redir_node(redir_pos, right_node, data));
-	return (create_redir_node(redir_pos, left_node, data));
-}
-
